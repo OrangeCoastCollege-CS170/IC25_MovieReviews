ea0cb4273a56a23e88835fc07f84c5a2e54e754d
Badajoz <sbadajoz@student.cccd.edu>
2016-12-02 12:45:51 -0800

    Complete required functionality
    Reads the text file for movie ratings and prints the list as well as the
    average rating.

--- src/edu/orangecoastcollege/sbadajoz/ic25/MovieReviews.java
+++ src/edu/orangecoastcollege/sbadajoz/ic25/MovieReviews.java
@@ -1,4 +1,10 @@
 package edu.orangecoastcollege.sbadajoz.ic25;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 /*
 Badajoz, Seve
 CS A170
@@ -7,5 +13,23 @@ December 2, 2016
 IC25
 */
 public class MovieReviews {
-
+	public static void main(String[] args) {
+		File ratings = new File("MovieReviews.txt");
+		double avg = 0.0;
+		int counter = 0;
+		ArrayList<Double> list = new ArrayList<>();
+		try {
+			Scanner input = new Scanner(ratings);
+			while (input.hasNext()) {
+				list.add(input.nextDouble());
+				avg += list.get(counter);
+			}
+			for (Double double1 : list) {
+				System.out.println(double1);
+			}
+			System.out.println("Average rating: " + (avg / list.size()));
+		} catch (FileNotFoundException e) {
+			System.out.println("File not found");
+		}
+	}
 }
