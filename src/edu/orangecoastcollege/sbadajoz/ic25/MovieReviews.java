bb0f6de1e79e78f29b7d73438a32466902bbdc48
Badajoz <sbadajoz@student.cccd.edu>
2016-12-02 12:52:36 -0800

    Fix bug, format output

--- src/edu/orangecoastcollege/sbadajoz/ic25/MovieReviews.java
+++ src/edu/orangecoastcollege/sbadajoz/ic25/MovieReviews.java
@@ -2,6 +2,7 @@ package edu.orangecoastcollege.sbadajoz.ic25;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -15,6 +16,7 @@ IC25
 public class MovieReviews {
 	public static void main(String[] args) {
 		File ratings = new File("MovieReviews.txt");
+		DecimalFormat one = new DecimalFormat("0.#");
 		double avg = 0.0;
 		int counter = 0;
 		ArrayList<Double> list = new ArrayList<>();
@@ -23,11 +25,14 @@ public class MovieReviews {
 			while (input.hasNext()) {
 				list.add(input.nextDouble());
 				avg += list.get(counter);
+				counter++;
 			}
+			input.close();
 			for (Double double1 : list) {
 				System.out.println(double1);
 			}
-			System.out.println("Average rating: " + (avg / list.size()));
+			System.out.println("Average rating: " + one.format(avg
+					/ list.size()));
 		} catch (FileNotFoundException e) {
 			System.out.println("File not found");
 		}
